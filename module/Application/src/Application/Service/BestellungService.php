58ff40c5dc48554917a14914f49a8a0f304d674d
Achim <kramer-fuer-dhbw@zibra.de>
2016-04-14 23:01:43 +0200

    bestellen-Methode rudimetär befüllt, Speichern fehlt noch

--- module/Application/src/Application/Service/BestellungService.php
+++ module/Application/src/Application/Service/BestellungService.php
@@ -1,4 +1,7 @@
 <?php
+namespace Application\Service;
+
+use \Application\Entity\Bestellung;
 
 /**
  * Diese Service-Klasse ist dafür gedacht, sich umalles zu kümmern,
@@ -8,11 +11,22 @@ class BestellungService
 {
     /**
      * Funktion löst eine Bestellung aus
+     * @param string $bezeichnung
      * @param \Application\Entity\Material $material
      * @param integer $anzahl
      */
-    public function bestellen($material, $anzahl) {
+    public function bestellen($bezeichnung, $material, $anzahl) {
 
-        // TODO
+        // Neues Bestellung-Objekt erzeugen
+        $bestellung = new Bestellung();
+
+        // Befüllen mit den übergebenen Parametern
+        $bestellung->setMaterial($material);
+        $bestellung->setBezeichnung($bezeichnung);
+        $bestellung->setAnzahl($anzahl);
+        $bestellung->setStatus("neu");
+        $bestellung->setZeitErstellt(new \DateTime());
+
+        // TODO: Speichern
     }
 }
