d511b3024b4b9b1ed74b2099b0c07ab7ecf3b8c7
Achim <kramer-fuer-dhbw@zibra.de>
2016-04-14 22:47:06 +0200

    Leere Bestellung- und Material-Service eingeführt

---
+++ module/Application/src/Application/Service/BestellungService.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Diese Service-Klasse ist dafür gedacht, sich umalles zu kümmern,
+ * das mit Bestellungen zu tun hat.
+ */
+class BestellungService
+{
+    /**
+     * Funktion löst eine Bestellung aus
+     * @param \Application\Entity\Material $material
+     * @param integer $anzahl
+     */
+    public function bestellen($material, $anzahl) {
+
+        // TODO
+    }
+}
---
+++ module/Application/src/Application/Service/MaterialService.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Diese Service Klasse ist dafür gedacht, sich um alles zu kümmern,
+ * das mit Materialien zu tun hat.
+ */
+class MaterialService
+{
+    /**
+     * Liefert alle bestellbaren Materialien
+     * @return array
+     */
+    public function getMaterialien() {
+
+        // TODO
+        return array();
+    }
+}
